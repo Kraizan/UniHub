b98160795225340a99435246e5323a8f96a825c6
Vanshaj Bhatnagar <vanshabhatnagar10@gmail.com>
2024-10-19 03:45:43 +0530

    added rabbitmq configurations

---
+++ reviewms/src/main/java/com/kraizan/reviewms/review/messaging/RabbitMQConfig.java
@@ -0,0 +1,32 @@
+package com.kraizan.reviewms.review.messaging;
+
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public Queue sellerRatingQueue(){
+        return new Queue("sellerRatingQueue");
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+}
